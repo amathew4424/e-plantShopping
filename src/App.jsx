46e42c976f73ea5eca7edcad5d7ecd7687b7d417
Allen Mathew <amathew4424@gmail.com>
2024-08-19 05:09:02 -0400

    improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,35 @@ function App() {
     setShowProductList(true);
   };
 
+  const showLandingPage = (e) => {
+    setShowProductList(false);
+  };
+
   return (
     <div className="app-container">
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
+
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
          
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
+
+          <div className="">
           <AboutUs/>
           </div>
-          </div>
+          
+        </div>
 
       </div>
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList />
+        <ProductList showLandingPage={(e)=>showLandingPage(e)} />
       </div>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,7 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 import { useDispatch, useSelector } from 'react-redux';
-function ProductList() {
+function ProductList({showLandingPage}) {
     const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -274,7 +274,7 @@ const handlePlantsClick = (e) => {
             <div className="tag">
                <div className="luxury">
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-               <a href="/" style={{textDecoration:'none'}}>
+               <a href="javascript:void(0)" onClick={showLandingPage} style={{textDecoration:'none',padding: '10px',}}>
                         <div>
                     <h3 style={{color:'white'}}>Paradise Nursery</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
@@ -316,7 +316,7 @@ const handlePlantsClick = (e) => {
                                         <img className='product-image' src={plant.image} alt='plant.name'/>
                                         <div className='product-title'>{plant.name}</div>
                                         <div className='product-description'>{plant.description}</div>
-                                        <div className='product-cost'>{plant.cost}</div>
+                                        <div className='product-price'>{plant.cost}</div>
                                         <button className={'product-button ' + (addedToCart[plant.name]?"added-to-cart":"") }
                                             onClick={()=>handleAddToCart(plant)}
                                         >
