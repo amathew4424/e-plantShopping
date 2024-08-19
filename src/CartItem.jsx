e1a3c6cb3b992693f49dbd6286908e37a5e842dc
Allen Mathew <amathew4424@gmail.com>
2024-08-19 04:36:19 -0400

    completed implementation of redux fns and cart

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
@@ -9,30 +9,49 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    console.log(cart);
     return cart.reduce(
       (total,item) => total + (Number(item.cost.replace('$','')) * item.quantity)
     , 0);
   }
 
   const handleContinueShopping = (e) => {
-    onContinueShopping();
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    if(item.quantity<10){
+      
+      dispatch(updateQuantity({
+        name:item.name,
+        quantity:item.quantity+1
+      }));
+    }
+    else
+      alert('Max quantity reached');
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+      dispatch(updateQuantity({
+        name:item.name,
+        quantity:item.quantity-1
+      }));
+    }
+    else
+      dispatch(removeItem(item));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return Number(item.cost.replace('$','')) * item.quantity;
   };
 
   return (
@@ -60,7 +79,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping()}>Checkout</button>
       </div>
     </div>
   );
